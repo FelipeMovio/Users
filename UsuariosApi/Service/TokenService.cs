1eb209626f8e156c942c170fa70b505e2f0a0577
Felipe Movio <felipelsmovio@gmail.com>
2026-08-15 11:34:35 -0300

    Analisando o Token

--- UsuariosApi/Service/TokenService.cs
+++ UsuariosApi/Service/TokenService.cs
@@ -17,6 +17,7 @@ public class TokenService
                 new Claim("username", usuario.UserName),
                 new Claim("id", usuario.Id),
                 new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString()),
+                new Claim("loginTimestamp",DateTime.UtcNow.ToString())
 
         };
 
